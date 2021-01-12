7211c2e7f8c0ab9b11f758d72a2b728117a5bb5c
Daniel Tanner-Davies <daniel.tannerdavies@gmail.com>
2021-01-12 11:05:10 +0000

    Add Objective C target as dependecy

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "okta-ios-jwt",
-            dependencies: [],
+            dependencies: ["okta-ios-jwt-objc"],
             path: "SourcesSwift",
             exclude: ["ThirdParty/JSONWebToken/Info.plist"],
             resources: [.copy("ThirdParty/JSONWebToken/LICENCE")]),
